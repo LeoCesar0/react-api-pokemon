d6c80b9bacae96a34f8b048641312dc2d5e99077
Leonardo César <leo_cesa@hotmail.com>
2021-09-15 18:30:38 -0300

    feat: create pokemon state

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import api from "../../services/api";
+import { useState } from "react";
 
 import PokemonItem from "../../components/PokemonItem";
 import PokemonLogo from "../../assets/images/pokemon.png"
@@ -6,11 +7,19 @@ import PokemonLogo from "../../assets/images/pokemon.png"
 import styles from './styles.module.css'
 
 function Home() {
-  api.get('pokemon')
+  const [pokemons, setPokemons] = useState()
+
+  const getAllPokemons = async () => {
+    const { data } = await api.get('pokemon')
+    
+    setPokemons(data.results)
+  }
+
+  console.log({ pokemons })
 
   return (
     <div className={styles.container}>
-      <img src={PokemonLogo} alt="" className={styles.logo} />
+      <img src={PokemonLogo} alt="" className={styles.logo} onClick={() => getAllPokemons()} />
       <h1 className={styles.title}>Meu site React</h1>
       <PokemonItem name="Pikachu" />
       <PokemonItem name="Charmander" />
