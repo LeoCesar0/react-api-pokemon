6e8d730c730044b9ae54bba73f88e117885356dd
Leonardo César <leo_cesa@hotmail.com>
2021-09-14 22:34:56 -0300

    feat: first Component created

---
+++ src/components/PokemonItem.tsx
@@ -0,0 +1,11 @@
+
+
+function PokemonItem(){
+    return (
+        <div>
+            <h2>Nome do Pokemon</h2>
+        </div>
+    )
+}
+
+export default PokemonItem
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import PokemonItem from "../components/PokemonItem";
 
 function App() {
   return (
     <div >
       <h1>Meu site React</h1>
+      <PokemonItem/>
     </div>
   );
 }
