9112020ea270e1de1cf74a653d2dd98c6ced13ba
Leonardo César <leo_cesa@hotmail.com>
2021-09-16 12:29:50 -0300

    feat(Pokemon): create iPokemon interface

--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -5,12 +5,30 @@ import { useParams } from "react-router";
 import styles from "./styles.module.css";
 
 interface IPokemon {
+  name: string;
+  weight: number;
+  height: number;
+  id: number;
+  species: { name: string; url: string };
+  base_experience: number;
+  types: { type: { name: string }; url: string }[];
+  abilities: { ability: { name: string; url: string }; slot: number }[];
+  sprites: {
+    front_default: string;
+    other: {
+      dream_world: { front_default: string };
+      official_artwork: { front_default: string };
+    };
+  };
+}
+
+interface IParams {
   pokemon: string;
 }
 
 const Pokemon: React.FC = () => {
-  const [pokemon, setPokemon] = useState();
-  const { pokemon: pokemonName } = useParams<IPokemon>();
+  const [pokemon, setPokemon] = useState<IPokemon>();
+  const { pokemon: pokemonName } = useParams<IParams>();
 
   const getPokemonData = async () => {
     const { data } = await api.get(`pokemon/${pokemonName}`);
@@ -26,8 +44,10 @@ const Pokemon: React.FC = () => {
 
   return (
     <div>
-      <h1 className={styles.title}>Pagina do pokemon: {pokemonName}</h1>
-      <p>Habilidade: </p>
+      <h1 className={styles.title} onClick={() => getPokemonData()}>
+        Pagina do pokemon: {pokemonName}
+      </h1>
+      <p>Habilidade: {pokemon?.abilities[0].ability.name}</p>
     </div>
   );
 };
